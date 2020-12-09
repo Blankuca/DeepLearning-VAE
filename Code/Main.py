408aefffbf340fe1e68f25cdbd937680376c5070
Blankuca <46654888+Blankuca@users.noreply.github.com>
2020-12-09 11:38:19 +0100

    WIP

--- Code/Main.py
+++ Code/Main.py
@@ -1,9 +1,10 @@
 
 import numpy as np
 
-from Dataloader import dataloader
-from MakeDict import findEDF
-from MNEplotter import MNEPlotter
+
+from Code.MakeDict import findEDF
+from Code.MNEplotter import MNEPlotter
+from Code.Dataloader import dataloader
 
 
 def make_batch(IDXlist, size=24, Nlable=6):
@@ -30,20 +31,21 @@ def make_batch(IDXlist, size=24, Nlable=6):
 
     return windowlist, filelist
 
-data_path=r"C:\Users\Andre\Desktop\Deeplearning local\artifact_dataset\artifact_dataset"
-path=r"artifact_dataset"
-idx=[0]
-edfDict=findEDF(DataDir=data_path)
-#annoIDX=np.load('testIDX.npy',allow_pickle=True)
-loader=dataloader(Time_interval=1,Overlap=0.25,Data_paht=data_path)
-edfDict,annothlist=loader.anno_mapping(edfDict,index=idx)
+def main():
+    data_path=r"C:\Users\Andre\Desktop\Deeplearning local\artifact_dataset\artifact_dataset"
+    path=r"artifact_dataset"
+    idx=[0]
+    edfDict=findEDF(DataDir=data_path)
+    #annoIDX=np.load('testIDX.npy',allow_pickle=True)
+    loader=dataloader(Time_interval=1,Overlap=0.25,Data_paht=data_path)
+    edfDict,annothlist=loader.anno_mapping(edfDict,index=idx)
 
-#data=loader.loadDict(edfDict=edfDict,index=idx)
+    #data=loader.loadDict(edfDict=edfDict,index=idx)
 
 
-windowslist,filelist=make_batch(IDXlist=annothlist,size=1,Nlable=1)
+    windowslist,filelist=make_batch(IDXlist=annothlist,size=1,Nlable=1)
 
-Batch,Batch_X,Batch_Y=loader.loadBatch(edfDict=edfDict,windowlist=windowslist,filelist=filelist)
-pl=MNEPlotter(CH_names=loader.CH_names,lableEncoding=loader.one_hot_eoncoding)
-pl.plot_raw(Batch_X[0],Batch_Y[0])
+    Batch,Batch_X,Batch_Y=loader.loadBatch(edfDict=edfDict,windowlist=windowslist,filelist=filelist)
+    pl=MNEPlotter(CH_names=loader.CH_names,lableEncoding=loader.one_hot_eoncoding)
+    pl.plot_raw(Batch_X[0],Batch_Y[0])
 
